010d4c2e189cb5f31f77545cea026600b10edf31
gbelenky <26817885+gbelenky@users.noreply.github.com>
2022-12-23 15:15:06 +0100

    adding user information

---
+++ Client/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Client
+{
+    public class User
+    {
+        public string identityProvider { get; set; }
+        public string userId { get; set; }
+        public string userDetails { get; set; }
+        public string userRoles { get; set; }
+        public (string, string)[] claims { get; set; }
+    }
+}
