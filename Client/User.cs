34ea904d38ff48a1ad2371d5fb24304544a70167
gbelenky <26817885+gbelenky@users.noreply.github.com>
2022-12-23 18:41:54 +0100

    extended User class

--- Client/User.cs
+++ Client/User.cs
@@ -5,7 +5,13 @@
         public string identityProvider { get; set; }
         public string userId { get; set; }
         public string userDetails { get; set; }
-        public string userRoles { get; set; }
-        public (string, string)[] claims { get; set; }
+        public string[] userRoles { get; set; }
+        public Claim[] claims { get; set; }
+    }
+
+    public class Claim
+    {
+        public string typ { get; set; }
+        public string val { get; set; }
     }
 }
