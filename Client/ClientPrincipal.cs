74de49519ffe940e1bd833619454ff4ea54e33bd
gbelenky <26817885+gbelenky@users.noreply.github.com>
2022-12-25 19:30:07 +0100

    introdusing client principal

--- Client/ClientPrincipal.cs
+++ Client/ClientPrincipal.cs
@@ -1,17 +1,17 @@
 ﻿namespace Client
 {
+
     public class User
+    {
+        public Clientprincipal clientPrincipal { get; set; }
+    }
+
+    public class Clientprincipal
     {
         public string identityProvider { get; set; }
         public string userId { get; set; }
         public string userDetails { get; set; }
         public string[] userRoles { get; set; }
-        public Claim[] claims { get; set; }
     }
 
-    public class Claim
-    {
-        public string typ { get; set; }
-        public string val { get; set; }
-    }
 }
