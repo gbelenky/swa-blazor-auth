962c83a2a1ee3434e63f1da089c3820ab376b43f
gbelenky <26817885+gbelenky@users.noreply.github.com>
2022-12-27 10:13:58 +0100

    fixing .auth.me

--- Client/ClientPrincipal.cs
+++ Client/ClientPrincipal.cs
@@ -8,10 +8,17 @@
 
     public class Clientprincipal
     {
-        public string identityProvider { get; set; }
         public string userId { get; set; }
-        public string userDetails { get; set; }
         public string[] userRoles { get; set; }
+        public Claim[] claims { get; set; }
+        public string identityProvider { get; set; }
+        public string userDetails { get; set; }
+    }
+
+    public class Claim
+    {
+        public string typ { get; set; }
+        public string val { get; set; }
     }
 
 }
