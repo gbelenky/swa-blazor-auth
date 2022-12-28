0db9ee0a942c4dcf9219918c30bb9305d5f5b496
gbelenky <26817885+gbelenky@users.noreply.github.com>
2022-12-28 20:35:31 +0100

    fixing the app settings

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -12,9 +12,9 @@ public class Startup : FunctionsStartup
     {
          
         builder.Services.AddSingleton<IProductData, ProductData>();
-        var clientId = Environment.GetEnvironmentVariable("ClientId");
-        var clientSecret = Environment.GetEnvironmentVariable("ClientSecret");
-        var tenantId = Environment.GetEnvironmentVariable("TenantId"); 
+        var clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+        var clientSecret = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
+        var tenantId = Environment.GetEnvironmentVariable("AAD_TENANT_ID"); 
         builder.Services.AddTransient<IAuthenticatedGraphClientFactory>(sp =>
     new AuthenticatedGraphClientFactory(
         // The parameters can be sourced from the Azure AD App Registration
