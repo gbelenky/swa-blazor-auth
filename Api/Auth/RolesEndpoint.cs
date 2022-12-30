03494971019f2921d86c4f1e97a9f611a96c507a
gbelenky <26817885+gbelenky@users.noreply.github.com>
2022-12-30 10:22:03 +0100

    experimenting with auth-roles function

---
+++ Api/Auth/RolesEndpoint.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using System.Linq;
+
+namespace Api.Auth
+{
+    public static class RolesEndpoint
+    {
+        [FunctionName("RolesEndpoint")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "auth-roles")] HttpRequest req,
+            ILogger log)
+        {
+            ClaimsPrincipal principal = ClaimsService.Parse(req, log);
+            var identity = principal?.Identity;
+            var data = new
+            {
+                Name = identity?.Name ?? "",
+                AuthenticationType = identity?.AuthenticationType ?? "",
+                Claims = principal?.Claims.Select(c => new { c.Type, c.Value }),
+            };
+
+            return new OkObjectResult(data);
+        }
+    }
+}
