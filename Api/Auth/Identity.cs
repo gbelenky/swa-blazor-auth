390866cb57e09acd933fb8812db7aed53cdc43d7
gbelenky <26817885+gbelenky@users.noreply.github.com>
2023-01-02 12:48:14 +0100

    default identity provider is empty now

--- Api/Auth/Identity.cs
+++ Api/Auth/Identity.cs
@@ -2,7 +2,7 @@ namespace Api.Auth;
 
 public class Identity
 {
-    public string identityProvider { get; set; } = "aad";
+    public string identityProvider { get; set; } = String.Empty;
     public string userId { get; set; } = String.Empty;
     public string userDetails { get; set; } = String.Empty;
     public string[] userRoles { get; set; } = new string[0];
