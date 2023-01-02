b3f100d267c1896f8a0ff87cc01ac4ff6d55eecb
gbelenky <26817885+gbelenky@users.noreply.github.com>
2023-01-02 13:04:05 +0100

    really - add?

--- Api/Auth/Identity.cs
+++ Api/Auth/Identity.cs
@@ -2,7 +2,7 @@ namespace Api.Auth;
 
 public class Identity
 {
-    public string identityProvider { get; set; } = String.Empty;
+    public string identityProvider { get; set; } = "aad";
     public string userId { get; set; } = String.Empty;
     public string userDetails { get; set; } = String.Empty;
     public string[] userRoles { get; set; } = new string[0];
